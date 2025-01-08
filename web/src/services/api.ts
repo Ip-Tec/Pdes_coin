40c92afba6df6663a9cf6f9811d0b3d7b7973abd
Peter Otakhor <otakhorpeter@gmail.com>
2025-01-08 19:59:09 +0100

    testing back-end url

--- web/src/services/api.ts
+++ web/src/services/api.ts
@@ -3,7 +3,7 @@ import { ErrorResponse, ResetPassword, User } from "../utils/type";
 
 // Create API instance
 const API = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000/api",
+  baseURL: import.meta.env.VITE_API_URL || "https://pdes-coin.onrender.com/api",
   headers: {
     "Content-Type": "application/json",
     Accept: "application/json",
