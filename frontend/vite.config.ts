472053f0d1abbcffcfc067a91e356b1f527c65c5
Peter Otakhor <otakhorpeter@gmail.com>
2025-02-11 19:43:53 +0100

    update

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://102.210.146.148',
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
+});
