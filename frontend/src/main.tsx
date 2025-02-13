0e81598c159caf7b22aec77d20f37d703374bc7b
Peter Otakhor <otakhorpeter@gmail.com>
2025-02-13 17:20:52 +0100

    sw fix

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -14,7 +14,7 @@ declare global {
 if ("serviceWorker" in navigator) {
   window.addEventListener("load", () => {
     navigator.serviceWorker
-      .register("./public/sw.js")
+      .register("../public/sw.js")
       .then((registration) => {
         console.log("Service Worker registered with scope:", registration.scope);
       })
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -20,12 +20,12 @@ export default defineConfig({
         start_url: "/login",
         icons: [
           {
-            src: "./src/assets/android-chrome-192x192.png",
+            src: "/android-chrome-192x192.png",
             sizes: "192x192",
             type: "image/png",
           },
           {
-            src: "./src/assets/android-chrome-512x512.png",
+            src: "/android-chrome-512x512.png",
             sizes: "512x512",
             type: "image/png",
           },
@@ -37,6 +37,7 @@ export default defineConfig({
         description: "Trade your cryptocurrency with PEDEX Coin.",
       },
     }),
+    
   ],
 
   server: {
