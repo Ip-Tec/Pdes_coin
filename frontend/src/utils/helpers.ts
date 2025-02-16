15cc406d7627f0b0500750183280deb6ebee6a9a
Peter Otakhor <otakhorpeter@gmail.com>
2025-02-16 16:37:44 +0100

    fix decimal places in balance

--- frontend/src/utils/helpers.ts
+++ frontend/src/utils/helpers.ts
@@ -6,11 +6,15 @@ export const formatCurrency = (amount: number): string => {
 };
 
 export const formattedMoneyUSD = (number: number): string => {
+  const truncated = Math.floor(number * 100) / 100; // Keep only two decimal places without rounding up
   return new Intl.NumberFormat("en-US", {
     style: "currency",
     currency: "USD",
-  }).format(number);
+    minimumFractionDigits: 2,
+    maximumFractionDigits: 2,
+  }).format(truncated);
 };
+
 export const formattedMoneyNGN = (amount: number) => {
   return new Intl.NumberFormat("en-NG", {
     style: "currency",
